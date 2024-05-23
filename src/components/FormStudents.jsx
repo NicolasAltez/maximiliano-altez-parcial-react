c3807b3d56e1462d2549cc0c074ca6d49f3637d6
Nicolas Altez <nicolasaltezacosta27@gmail.com>
2024-05-23 15:59:05 -0300

    initial commit

---
+++ src/components/FormStudents.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const FormStudents = () =>{
+    return (
+        <div>
+            <h1>Carga de estudiantes</h1>
+            <form></form>
+        </div>
+    )
+};
+
+export default FormStudents;
